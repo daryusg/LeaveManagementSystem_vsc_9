38fa6ead4500831f538ad35547f32c1cbef598b4
kev <kev@daryus.com>
2025-01-29 17:41:42 +0000

    cip...167 (Section Review) added leave requests module added complex queries and view models looked at efficient querying and coding techniques   abstracted repetitive operations to services   added complex validation logic   fixed bugs as they arose added javascript and jquery elements to enhance ui and usability reviewed best practices for coding and security concerns

--- LeaveManagementSystem.Web/obj/Debug/net9.0/LeaveManagementSystem.Web.AssemblyInfo.cs
+++ LeaveManagementSystem.Web/obj/Debug/net9.0/LeaveManagementSystem.Web.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeaveManagementSystem.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b81eb55961dddd8b4f36b5722bc13e4f4f656e49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e488c3aabbd352f317b2417e9c299bf5defb7128")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeaveManagementSystem.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeaveManagementSystem.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
